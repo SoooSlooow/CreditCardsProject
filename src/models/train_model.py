4a3aa628bde7da77702c1d9a93e78d84af0fc99e
Andrey Ovsyannikov <andmit8@mail.ru>
2022-09-07 20:20:42 +0300

    Add model training

--- src/models/train_model.py
+++ src/models/train_model.py
@@ -1,1 +1,35 @@
+import click
+import joblib
+import pandas as pd
+from lightgbm import LGBMClassifier
 from src.features.fs_utils import create_column_transformers, create_pipeline
+
+
+@click.command()
+@click.argument('input_data_path', type=click.Path(exists=True))
+@click.argument('output_model_path', type=click.Path())
+def train_model(input_data_path, output_model_path):
+    df = pd.read_csv(input_data_path)
+    X = df.drop(['BAD_CLIENT'], axis=1)
+    y = df['BAD_CLIENT']
+
+    column_transformer = create_column_transformers(X)['without_occupation_type']
+    model_params = {'lambda_l2': 31.142026352479732,
+                    'max_depth': 40,
+                    'n_estimators': 320,
+                    'learning_rate': 0.09130234251787786
+                    }
+    model = LGBMClassifier(random_state=0, **model_params)
+
+    pipeline = create_pipeline(column_transformer, model)
+    pipeline.fit(X, y)
+
+    joblib.dump(model, output_model_path)
+
+
+if __name__ == '__main__':
+    train_model()
+
+'''
+python -m src.models.train_model data/interim/cleaned_dataset.csv models/final_model.pkl
+'''
